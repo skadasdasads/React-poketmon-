2c7432b60214dd608a716b910acb950e17a06de2
euuuuuuan <euan.may24@gmail.com>
2024-08-30 19:30:01 +0900

    Poketmon

---
+++ 13_REACT-main/05_router/03_params/src/App.js
@@ -0,0 +1,31 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Layout from './layouts/Layout';
+import Main from './pages/Main';
+import About from './pages/About';
+import Menu from './pages/Menu';
+import MenuDetails from './pages/MenuDetails';
+import MenuSearchResult from './pages/MenuSearchResult';
+
+
+function App() {
+  return (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<Main/>}/>
+        <Route path="main" element={<Main/>}/>
+        <Route path="about" element={<About/>}/>
+        <Route path="menu">
+          <Route index element={<Menu/>}/>
+          {/* :menuCode => pathVariable를 사용해 값 전달 */}
+          <Route path=":menuCode" element={<MenuDetails/>}/>
+          {/* 쿼리스트링을 사용해 값을 전달 */}
+          <Route path="search" element={<MenuSearchResult/>}/>
+        </Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
